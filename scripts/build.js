fa09a723532aa7c66266be7d54cd3c745991545a
Ayanava De <ayanavade01@gmail.com>
2021-08-17 14:33:06 +0530

    fix: :ambulance: build script
    Correct working directory changes for build script.

--- scripts/build.js
+++ scripts/build.js
@@ -6,8 +6,11 @@ const path = "./wiki/";
 const directories = fs.readdirSync(path, {withFileTypes: true})
 				  .filter(f => f.isDirectory());
 
+shell.cd(path);
+
 for(const dir of directories) {
-	shell.cd(`${path}${dir.name}`);
+	shell.cd(dir.name);
 	shell.exec("tiddlywiki --build index");
 	fs.renameSync("output/index.html", "index.html");
+	shell.cd("..");
 }
